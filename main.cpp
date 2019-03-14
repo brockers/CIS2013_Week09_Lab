a2a3ef4c617a0c604f990977118ac1699e0330dd
BT228 <svc_OKC_BT228@ad.osuokc.edu>
2019-03-13 19:18:36 -0500

    Something is working... we should save

--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
+
 int main(){
+	
+	const int B_SIZE = 8;
+	char chess[B_SIZE][B_SIZE];
+	
+	for(int i=0;i<B_SIZE;i++){
+		for(int j=0;j<B_SIZE;j++){
+			chess[i][j] = '-';	
+		}
+	}
+	
+	for(int i=0;i<B_SIZE;i++){
+		cout << endl;
+		for(int j=0;j<B_SIZE;j++){
+			cout << chess[i][j] << " ";	
+		}
+	}
 
 	return 0;
 }
